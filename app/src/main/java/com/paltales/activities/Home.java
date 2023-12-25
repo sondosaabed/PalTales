9b5490fc3057b6d7ab4524f76732b1b8fcbaf551
Sondos <sondosahmedabed@gmail.com>
2023-12-26 00:45:54 +0200

    Create the main activity

---
+++ app/src/main/java/com/paltales/activities/Home.java
@@ -0,0 +1,58 @@
+package com.paltales.activities;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.paltales.R;
+
+/*
+This is the home activity where the users choses what they wanna see wether it's movies or books
+ */
+public class Home extends AppCompatActivity {
+    ImageButton moviesbtn;
+    ImageButton booksbtn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        initialize();
+    }
+
+    private void initialize() {
+        setBooksbtn(findViewById(R.id.books));
+        setMoviesbtn(findViewById(R.id.movies));
+
+        handle_books(getBooksbtn());
+        handle_movies(getMoviesbtn());
+    }
+
+    private void handle_movies(ImageButton moviesbtn) {
+        moviesbtn.setOnClickListener(e->{
+
+        });
+    }
+
+    private void handle_books(ImageButton booksbtn) {
+        booksbtn.setOnClickListener(e->{
+
+        });
+    }
+
+    /*
+    Getters & Setters
+     */
+    public ImageButton getMoviesbtn() {
+        return moviesbtn;
+    }
+    public void setMoviesbtn(ImageButton moviesbtn) {
+        this.moviesbtn = moviesbtn;
+    }
+    public ImageButton getBooksbtn() {
+        return booksbtn;
+    }
+    public void setBooksbtn(ImageButton booksbtn) {
+        this.booksbtn = booksbtn;
+    }
+}
