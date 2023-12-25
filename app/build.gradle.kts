65575a8279fa324ac92ed6fdcc9d1bf37ac4b745
Sondos <sondosahmedabed@gmail.com>
2023-12-25 20:26:42 +0200

    Add Volly Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.paltales"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.paltales"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -38,4 +38,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.android.volley:volley:1.2.1")
 }
