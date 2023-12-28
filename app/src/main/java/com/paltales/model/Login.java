810a53b5909fe2746a419e6a15af9622bfe93c24
Sondos <sondosahmedabed@gmail.com>
2023-12-29 01:23:33 +0200

    Create Login.java

--- app/src/main/java/com/paltales/model/Login.java
+++ app/src/main/java/com/paltales/model/Login.java
@@ -1,4 +1,27 @@
 package com.paltales.model;
-
+/*
+This login Object class will be used in logins
+ */
 public class Login {
+    private String userName;
+    private String password;
+
+    public Login(String userName, String password){
+
+    }
+    /*
+        Getters & Setters
+     */
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
