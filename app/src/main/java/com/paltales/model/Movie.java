98b396f42e06385f3f0a2d315e0eb479ae21f286
Sondos <sondosahmedabed@gmail.com>
2023-12-29 05:42:00 +0200

    Finish the Movie class

--- app/src/main/java/com/paltales/model/Movie.java
+++ app/src/main/java/com/paltales/model/Movie.java
@@ -8,81 +8,66 @@ public class Movie {
     int ratingValue;
     String genre; //"Documentary"
     String datePublished; //"2016-10-16"
-    String keywords; //"keywords": "infotainment,perception,deception,reference to george orwell,reagan doctrine"
     String director; //"name": "Adam Curtis"
 
-
+    public Movie(String url, String name, String image, String description, int ratingValue, String genre, String datePublished, String director) {
+        setUrl(url);
+        setName(name);
+        setImage(image);
+        setDescription(description);
+        setRatingValue(ratingValue);
+        setGenre(genre);
+        setDatePublished(datePublished);
+        setDirector(director);
+    }
     /*
-    Getters & Setters
-     */
+       Getters & Setters
+    */
     public String getUrl() {
         return url;
     }
-
     public void setUrl(String url) {
         this.url = url;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getImage() {
         return image;
     }
-
     public void setImage(String image) {
         this.image = image;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public int getRatingValue() {
         return ratingValue;
     }
-
     public void setRatingValue(int ratingValue) {
         this.ratingValue = ratingValue;
     }
-
     public String getGenre() {
         return genre;
     }
-
     public void setGenre(String genre) {
         this.genre = genre;
     }
-
     public String getDatePublished() {
         return datePublished;
     }
-
     public void setDatePublished(String datePublished) {
         this.datePublished = datePublished;
     }
-
-    public String getKeywords() {
-        return keywords;
-    }
-
-    public void setKeywords(String keywords) {
-        this.keywords = keywords;
-    }
-
     public String getDirector() {
         return director;
     }
-
     public void setDirector(String director) {
         this.director = director;
     }
