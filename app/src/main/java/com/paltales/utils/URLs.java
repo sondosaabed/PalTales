be1a516c120d7d9c437c268c799aa00172b72307
Sondos <sondosahmedabed@gmail.com>
2023-12-29 11:13:03 +0200

    Add books url

--- app/src/main/java/com/paltales/utils/URLs.java
+++ app/src/main/java/com/paltales/utils/URLs.java
@@ -5,5 +5,5 @@ public class URLs {
         This is like an enum I use it to edit links URLs in one palce
      */
     public static final String MOVIES_URL = "https://www.imdb.com/list/ls563010565/?sort=alpha,asc&st_dt=&mode=detail&page=1";
-    public static final String BOOKS_URL = "";
+    public static final String BOOKS_URL = "https://openlibrary.org/people/sondos_aabed/lists/OL243427L/Palestinian/export?format=json";
 }
