a74d368a4b4db4beef04b6ff3c57a6812a5b46df
Sondos <sondosahmedabed@gmail.com>
2023-12-29 11:58:01 +0200

    Edit movie class (Remove unneccessary)

--- app/src/main/java/com/paltales/model/Movie.java
+++ app/src/main/java/com/paltales/model/Movie.java
@@ -4,21 +4,13 @@ public class Movie {
     String url; //"url": "https://www.imdb.com/title/tt6156350/"
     String name; //"name": "HyperNormalisation",
     String image; //"image": "https://m.media-amazon.com/images/M/MV5BNWFlNDlkYWMtODEyOC00MjY1LWIyM2EtMzVjNjQ2YmM4NzE4XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
-    String description; //"description": "Adam Curtis explains how, at a time of confusing and inexplicable world events, politicians and the people they represent have retreated in to a damaging over-simplified version of what is happening.",
     int ratingValue;
-    String genre; //"Documentary"
-    String datePublished; //"2016-10-16"
-    String director; //"name": "Adam Curtis"
 
-    public Movie(String url, String name, String image, String description, int ratingValue, String genre, String datePublished, String director) {
+    public Movie(String url, String name, String image, int ratingValue) {
         setUrl(url);
         setName(name);
         setImage(image);
-        setDescription(description);
         setRatingValue(ratingValue);
-        setGenre(genre);
-        setDatePublished(datePublished);
-        setDirector(director);
     }
     /*
        Getters & Setters
@@ -41,34 +33,10 @@ public class Movie {
     public void setImage(String image) {
         this.image = image;
     }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
     public int getRatingValue() {
         return ratingValue;
     }
     public void setRatingValue(int ratingValue) {
         this.ratingValue = ratingValue;
-    }
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    public String getDatePublished() {
-        return datePublished;
-    }
-    public void setDatePublished(String datePublished) {
-        this.datePublished = datePublished;
-    }
-    public String getDirector() {
-        return director;
-    }
-    public void setDirector(String director) {
-        this.director = director;
     }
 }
