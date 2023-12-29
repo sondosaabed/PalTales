de843130a0b71227c67fa85da16fb31a831ff3a1
Sondos <sondosahmedabed@gmail.com>
2023-12-29 18:11:26 +0200

    Create welcome back activity

---
+++ app/src/main/java/com/paltales/activities/WelcomeBack.java
@@ -0,0 +1,57 @@
+package com.paltales.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.paltales.R;
+import com.paltales.config.Preferences;
+
+/*
+    This is another starting activity of the mobile app,
+    Checking if it's not the first time of the user then welcome them back
+ */
+public class WelcomeBack extends AppCompatActivity {
+    /*
+        Attributes
+     */
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initialize();
+    }
+
+    private void initialize() {
+        if(getSupportActionBar()!=null)
+            getSupportActionBar().hide();
+        setContentView(R.layout.welcome_back);
+        setStart(findViewById(R.id.start));
+        handle_start(getStart());
+    }
+
+    private void handle_start(Button start) {
+        start.setOnClickListener(view -> {
+            Intent intent;
+            if(Preferences.isRememberMe(this)) {
+                intent = new Intent(this, Home.class);
+            }else{
+                intent = new Intent(this, LoginActivity.class);
+            }
+            startActivity(intent);
+        });
+    }
+
+    /*
+        Getters & Setters
+     */
+    public Button getStart() {
+        return start;
+    }
+    public void setStart(Button start) {
+        this.start = start;
+    }
+}
