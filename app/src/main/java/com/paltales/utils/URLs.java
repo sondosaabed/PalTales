646594453e8a573d9052e0ae9c1401b4e91a4f34
Sondos <sondosahmedabed@gmail.com>
2024-01-01 06:09:40 +0200

    Update movies URL

--- app/src/main/java/com/paltales/utils/URLs.java
+++ app/src/main/java/com/paltales/utils/URLs.java
@@ -4,6 +4,6 @@ public class URLs {
     /*
         This is like an enum I use it to edit links URLs in one palce
      */
-    public static final String MOVIES_URL = "https://www.imdb.com/list/ls563010565/?st_dt=&mode=detail&page=1&sort=alpha,asc&genres=Drama&ref_=ttls_ref_gnr";
+    public static final String MOVIES_URL = "https://raw.githubusercontent.com/sondosaabed/Palestinian-Movies-JSON-Dataset/main/palestinian_movies.json";
     public static final String BOOKS_URL = "https://openlibrary.org/people/sondos_aabed/lists/OL243427L/Palestinian/export?format=json";
 }
