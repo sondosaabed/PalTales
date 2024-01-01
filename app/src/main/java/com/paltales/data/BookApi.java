fa8646ff643951cfcc2fb7afd796159287fec190
Sondos <sondosahmedabed@gmail.com>
2024-01-02 01:32:10 +0200

    Refactor: Remove unused imports

--- app/src/main/java/com/paltales/data/BookApi.java
+++ app/src/main/java/com/paltales/data/BookApi.java
@@ -3,8 +3,6 @@ package com.paltales.data;
 import android.content.Context;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.paltales.model.Book;
