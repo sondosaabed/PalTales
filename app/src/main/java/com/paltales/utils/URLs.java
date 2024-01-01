5265e6a8d8bcb5d798e5959c80598047f84b8520
Sondos <sondosahmedabed@gmail.com>
2024-01-01 22:20:58 +0200

    Refactor: Documentations & some renames

--- app/src/main/java/com/paltales/utils/URLs.java
+++ app/src/main/java/com/paltales/utils/URLs.java
@@ -1,9 +1,7 @@
 package com.paltales.utils;
 
 public class URLs {
-    /*
-        This is like an enum I use it to edit links URLs in one palce
-     */
+    /* Web Service URLS */
     public static final String MOVIES_URL = "https://raw.githubusercontent.com/sondosaabed/Palestinian-Movies-JSON-Dataset/main/palestinian_movies.json";
     public static final String BOOKS_URL = "https://openlibrary.org/people/sondos_aabed/lists/OL243427L/Palestinian/export?format=json";
 }
